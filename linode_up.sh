94f36b47c9ef51fb2a948db9b997c3e23150235f
Adimir Colen <adimircolen@gmail.com>
2011-02-06 13:17:37 -0200

    comandos para ajustar e install passanger-nginx

--- linode_up.sh
+++ linode_up.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 echo "Script para subir uma maquina com ruby compilado e banco de dados"
 `cd root`
-read
+if (($?)) ; then read ; fi
 echo "### Update no sistema ###"
 read
 echo `apt-get update -y`
 echo `apt-get upgrade --show-upgraded -y`
 echo "Instalando htop, mysql, pacotes para compilar codigo fonte"
 read
-echo`apt-get install -y mysql-server htop build-essential autoconf`
+echo`apt-get install -y mysql-server htop build-essential autoconf libcurl4-openssl-dev`
 echo "Criar uma parta no home do root para arquivar os fontes"
 read
 echo `mkdir fonts`
@@ -18,10 +18,12 @@ read
 `tar -zxvf ruby-1.8.7-p174.tar.gz;cd ruby-1.8.7-p174`
 echo "Conpilando ..."
 read
-`./configure;make;make test`
+exec './configure --with-openssl-dir=/usr/lib'
+exec 'make'
+exec 'make test'
 echo "Instalando ..."
 read
-`make install`
+exec 'make install'
 echo "Foi compilado e instalado o ruby na versão "`ruby -v`
 echo "Instalando zlib ..."
 read
@@ -34,3 +36,4 @@ read
 echo "Instalando rubygems ..."
 read
 `ruby setup.rb`
+`passenger-install-nginx-module`
